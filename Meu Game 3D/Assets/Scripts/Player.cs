5d9a490ba143395a48bc6a59148e89c55f51b079
FernandoSantos0920 <fernando.santos2@escolar.ifrn.edu.br>
2024-08-20 14:27:58 -0300

    Câmera seguindo

--- Meu Game 3D/Assets/Scripts/Player.cs
+++ Meu Game 3D/Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Vector3 = UnityEngine.Vector3;
 
 public class Player : MonoBehaviour
@@ -11,18 +12,30 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("START");
+        
         TryGetComponent(out rb);
     }
 
     // Update is called once per frame
     void Update()
-    {
-      float h = Input.GetAxis("Horizontal");  
-      float v = Input.GetAxis("Vertical");    
+    { 
+        
+        Debug.Log("UPDATE"); 
+      float h = Input.GetAxis("Horizontal"); // -1 esquerda, 0 nada, 1 direita
+      float v = Input.GetAxis("Vertical"); // -1 pra tras, 0 nada, 1 pra frente
       Vector3 direcao = new Vector3(h, 0, v);
       rb.AddForce(direcao * velocidade * Time.deltaTime, ForceMode.Impulse);
 
 
+      if (transform.position.y <= -10)
+      {
+          //Jogador caiu do mundo
+          SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+      }
+      
+      
+      
 
     }
 }
